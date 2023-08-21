fc8cf34425b8fdaf707e77f79a3382dc4996d904
Matheus Milanez <mattmila96@gmail.com>
2023-08-21 16:04:25 -0300

    requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,20 @@
+def is_palindrome_recursive_aux(word, low_index, high_index):
+    if low_index == high_index:
+        return word[0]
+    else:
+        new_word = word[high_index] + is_palindrome_recursive_aux(
+            word, low_index, high_index - 1)
+        return new_word
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word is None or len(word) == 0:
+        return False
+    if low_index == high_index:
+        return True
+    new_word = word[high_index] + is_palindrome_recursive_aux(
+        word, low_index, high_index - 1)
+    return new_word == word
+
+
+print(is_palindrome_recursive("", 0, 0))
