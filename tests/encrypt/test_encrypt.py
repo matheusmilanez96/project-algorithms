09fa94da02086ae2be7f40cbdd8f6a2e55dc0f01
Matheus Milanez <mattmila96@gmail.com>
2023-08-21 15:29:31 -0300

    requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "3")
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 2) == "egass_em"
+    assert encrypt_message("message", 22) == "egassem"
