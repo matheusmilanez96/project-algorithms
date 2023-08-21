e9dfa99133bf952556837bf08d402fafb7e36402
Matheus Milanez <mattmila96@gmail.com>
2023-08-21 14:58:17 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
-# primeiro commit
+    if target_time is None:
+        return None
+    counter = 0
+    for times in permanence_period:
+        if (type(times[0]) != int or type(times[1]) != int):
+            return None
+        if times[0] <= target_time <= times[1]:
+            counter += 1
+    return counter
